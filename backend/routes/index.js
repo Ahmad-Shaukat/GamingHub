9e1b565a01a2b12fc46e4a50fe62a1cacbf38d4e
ahmad-shaukat <ahmadshaukat@yahoo.com>
2023-09-26 23:58:27 -0400

    added routes

--- backend/routes/index.js
+++ backend/routes/index.js
@@ -1,5 +1,8 @@
 const express = require('express');
 const router = express.Router();
+const apiRouter = require('./api')
+
+router.use('/api', apiRouter)
 
 router.get('/api/csrf/restore', function(req, res) {
     const csrfToken = req.csrfToken()
@@ -8,6 +11,9 @@ router.get('/api/csrf/restore', function(req, res) {
         'XSRF-Token':csrfToken
     })
     res.send('Hello World!')
+})
+router.post('/test', function (req, res) {
+    res.json({requestBody: req.body})
 })
 
 module.exports = router;
