cc923eca219c3615d0bb7b83513a9d22b0bf9f51
ahmad-shaukat <ahmadshaukat@yahoo.com>
2023-09-30 20:45:45 -0400

    added error handling routes

--- backend/app.js
+++ backend/app.js
@@ -1,42 +1,57 @@
-const express = require('express');
-const routes = require('./routes')
-require('express-async-errors');
-const morgan = require('morgan')
-const cors = require('cors')
-const csurf = require('csurf')
-const helmet = require('helmet')
-const cookieParser = require('cookie-parser')
-const {environment} = require('./config')
-const isProduction = environment === 'production'
-const app = express()
-app.use(morgan('dev'))
+const express = require("express");
+const routes = require("./routes");
+require("express-async-errors");
+const morgan = require("morgan");
+const cors = require("cors");
+const csurf = require("csurf");
+const helmet = require("helmet");
+const cookieParser = require("cookie-parser");
+const { environment } = require("./config");
+const isProduction = environment === "production";
+const app = express();
+app.use(morgan("dev"));
 // app.use(cs
-app.use(cookieParser())
-app.use(express.json())
+app.use(cookieParser());
+app.use(express.json());
 
 // Security Middleware
 if (!isProduction) {
-    // enable cors only in development
-    app.use(cors())
+  // enable cors only in development
+  app.use(cors());
 }
 
 // helmet helps set a variety of headers to better secure your app
 app.use(
-    helmet.crossOriginResourcePolicy({
-        policy: "cross-origin"
+  helmet.crossOriginResourcePolicy({
+    policy: "cross-origin",
+  })
+);
+app.use(
+  csurf({
+    cookie: {
+      secure: isProduction,
+      sameSite: isProduction && "Lax",
+      httpOnly: true,
+    },
+  })
+);
+app.use(routes);
+
+app.use((_req, _res, next) => {
+    const err = new Error("The requested resource couldn't be found.")
+    err.title = "Resource Not Found"
+    err.errors = {message: "The requested resource couldn't be found"}
+    err.status = 404;
+    next(err)
+})
+app.use((err, _req, res, _next) => {
+    res.status(err.status || 500)
+    console.error(err)
+    res.json({
+        title: err.title || 'Server Error',
+        message: err.message,
+        errors: err.errors,
+        stack: isProduction ? Null : err.stack
     })
-    );
-    app.use(
-        csurf({
-            cookie: {
-                secure: isProduction,
-                sameSite: isProduction && "Lax",
-                httpOnly: true
-            }
-        })
-        );
-        app.use(routes)
-        module.exports = app
-
-  
-
+})
+module.exports = app;
