32c40256a51e6c4712d00dd2c35cb99a4b8b6fb0
ahmad-shaukat <ahmadshaukat@yahoo.com>
2023-10-15 10:32:17 -0400

    add session stroe

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -4,19 +4,21 @@ import './index.css';
 import App from './App';
 import configureStore from './store';
 
-import { Provider } from 'react';
+import { Provider } from 'react-redux';
 import {BrowserRouter} from 'react-router-dom'
 import { restoreCSRF, csrfFetch } from './store/csrf';
+import * as sessionActions from './store/session'
 
 const store = configureStore()
+// if (process.env.NODE_ENV !== 'production') {
+//   window.store = store
+// }
 if (process.env.NODE_ENV !== 'production') {
-  window.store = store
-}
-if (process.env.NODE !== 'production') {
   restoreCSRF()
 
   window.csrfFetch = csrfFetch
   window.store = store
+  window.sessionActions = sessionActions;
 }
 
 function Root () {
