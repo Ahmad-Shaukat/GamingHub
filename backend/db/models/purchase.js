85fb0d3e95a9a3a19f6783a319484a0d30e32742
ahmad-shaukat <ahmadshaukat@yahoo.com>
2023-12-04 18:07:33 -0500

    added association to category

--- backend/db/models/purchase.js
+++ backend/db/models/purchase.js
@@ -11,6 +11,9 @@ module.exports = (sequelize, DataTypes) => {
      */
     static associate(models) {
       // define association here
+      Purchase.belongsTo(models.Category, {
+        foreignKey: categoryId, onDelete: 'CASCADE'
+      })
     }
   }
   Purchase.init(
