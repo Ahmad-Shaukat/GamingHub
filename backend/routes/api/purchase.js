7f13ebd7a1ab9edb42aba8a6f5c6793be480f8ba
ahmad-shaukat <ahmadshaukat@yahoo.com>
2024-01-05 23:00:04 -0500

    added delete api

--- backend/routes/api/purchase.js
+++ backend/routes/api/purchase.js
@@ -16,12 +16,13 @@ router.get('/current', restoreUser, async(req, res) => {
         where:{userId: userId}
     })
     console.log (userId, '---------------this is the user')
+    console.log (userPurchases, '-------------these are user purchases')
     res.status(200).json(userPurchases)
 })
 
 
 // add the purchase 
-router.post('/ye', requireAuth, async(req, res) => {
+router.post('/new', requireAuth, async(req, res) => {
     let userId = req.user.dataValues.id
     let {name, store, date, type, category} = req.body
 
@@ -38,6 +39,26 @@ router.post('/ye', requireAuth, async(req, res) => {
     console.log (categoryId, '---------this is the purchase category')
     console.log(newPurchase, 'this is the new purchase--------------')
     res.status(200).json(newPurchase)
+})
+
+// Delete a purchase 
+router.delete('/:purchaseId', requireAuth, async(req, res) => {
+    let userId = req.user.dataValues.id
+    let purchaseId = req.params.purchaseId
+    const purchaseValue = await Purchase.findByPk(purchaseId)
+    if (!purchaseValue) {
+        return res.status(404).json()({
+            'message': "purchase couldn't be found",
+            "statusCode": 404
+        })
+    }
+    console.log (purchaseValue.dataValues, '-----------this is the purchase value')
+
+    // await purchase.destroy()
+    res.status(200).json({
+        "message":"Successfully deleted",
+        'statusCode':200
+    })
 })
 
 module.exports = router
