e5bc5418643bb6e372a5dffc52cae9e725224a5e
ahmad-shaukat <ahmadshaukat@yahoo.com>
2024-01-06 23:09:47 -0500

    git api has bug where entire purchase table is deleted

--- backend/routes/api/purchase.js
+++ backend/routes/api/purchase.js
@@ -46,15 +46,16 @@ router.delete('/:purchaseId', requireAuth, async(req, res) => {
     let userId = req.user.dataValues.id
     let purchaseId = req.params.purchaseId
     const purchaseValue = await Purchase.findByPk(purchaseId)
+    console.log (purchaseValue, '--------------this is purchase value')
     if (!purchaseValue) {
-        return res.status(404).json()({
+        return res.status(404).json({
             'message': "purchase couldn't be found",
             "statusCode": 404
         })
     }
-    console.log (purchaseValue.dataValues, '-----------this is the purchase value')
+    // console.log (purchaseValue.dataValues, '-----------this is the purchase value')
 
-    // await purchase.destroy()
+    await purchaseValue.destroy()
     res.status(200).json({
         "message":"Successfully deleted",
         'statusCode':200
