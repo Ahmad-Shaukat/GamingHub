2e642b95c42ddff69689eb570ab0e815abc9c1e7
ahmad-shaukat <ahmadshaukat@yahoo.com>
2024-04-24 02:43:36 -0400

    add labels and inputs in the create purchase form

--- frontend/src/components/AddSpendingForm/index.js
+++ frontend/src/components/AddSpendingForm/index.js
@@ -1,9 +1,21 @@
 
 import React from "react"
-
+import { useState } from "react"
 
 
 const AddSpendingForm = () => {
+const [category, setCategory] = useState('')
+const [storeName, setStoreName] = useState('')
+const [amount, setAmount] = useState(0)
+
+const onSubmit = (e) => {
+    e.preventDefault()
+    let purchaseInfo = {
+        category, storeName, amount
+    }
+    console.log (purchaseInfo, '--------------this is new purchase') 
+}
+
 
     return (
 
@@ -12,6 +24,31 @@ const AddSpendingForm = () => {
                 <div>
                     <label>Store Name</label>
                     <input type="text"></input>
+                </div>
+                <div>
+                    <label>Category</label>
+                    <select>
+                        <option> Pick Category
+                        </option>
+                        <option value='shopping'> Shopping
+                        </option>
+                        <option value='food'> Food
+                        </option>
+                        <option value='bills'> Bills
+                        </option>
+                        <option value='savings'>
+                            Saving
+                        </option>
+                    </select>
+                </div>
+                <div>
+                    <label>Amount</label>
+                    <input type="number"></input>
+                </div>
+                <div>
+                    <button type="submit" onClick={onSubmit()}>
+                    Add 
+                    </button>
                 </div>
             </form>
         </div>
