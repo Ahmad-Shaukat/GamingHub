057291ab74f229ed58a3f83ab81566dfdf64383b
ahmad-shaukat <ahmadshaukat@yahoo.com>
2024-04-28 23:02:42 -0400

    add list migration, model and seed

--- backend/routes/api/index.js
+++ backend/routes/api/index.js
@@ -2,7 +2,6 @@ const router = require('express').Router()
 const sessionRouter = require('./session.js')
 const usersRouter = require('./users.js')
 const {restoreUser} = require('../../utils/auth.js')
-const purchaseRouter = require('./purchase.js')
 const profileRouter = require('./profile.js')
 
 // Connect restoreUser middleware to the API router
@@ -13,7 +12,6 @@ router.use(restoreUser)
 
 router.use('/session', sessionRouter);
 router.use('/users', usersRouter);
-router.use('/purchase', purchaseRouter)
 router.use('/profile', profileRouter)
 
 
